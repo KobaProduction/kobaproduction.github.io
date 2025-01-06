332e500d59fa68f77610d774c18bc466f2e44362
ArthurKoba <caprimores@gmail.com>
2025-01-06 19:40:35 +0300

    update layouts

--- src/app/ui/layouts/DefaultLayout.vue
+++ src/app/ui/layouts/DefaultLayout.vue
@@ -1,16 +1,22 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts" />
 
 <template>
-
   <q-layout view="hHh lpr lFr">
 
-    <slot name="header"/>
-    <slot name="sidebar"/>
+    <slot name="header">
+      <q-header reveal elevated />
+    </slot>
 
-    <slot name="page"/>
+    <slot name="sidebar">
+      <q-drawer />
+    </slot>
 
-    <slot name="footer"/>
+    <slot name="page" />
+
+    <slot name="footer">
+      <q-footer bordered class="footer" />
+    </slot>
+
   </q-layout>
 </template>
 
--- src/app/ui/layouts/EmptyLayout.vue
+++ src/app/ui/layouts/EmptyLayout.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts"/>
+<script setup lang="ts" />
 
 <template>
-  <q-layout view="hHh lpr lFr">
-    <slot name="page"/>
+  <q-layout>
+    <slot name="page" />
   </q-layout>
 </template>
 
